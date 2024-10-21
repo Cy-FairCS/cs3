f7aaad91948163f6f00febed01c84fc027f09d92
WeldonJasik <weldon.jasik@cfisd.net>
2024-10-21 07:16:04 -0500

    Update to Abstract Classes notes

--- u04b_abstractClasses/notes/InterfaceExtends.java
+++ u04b_abstractClasses/notes/InterfaceExtends.java
@@ -25,6 +25,14 @@ interface D extends A,B,C
 	
 }
 
+class W implements D
+{
+   // which methods must the W class implement?
+   public void fun() { out.println("fun");  }
+   public void notFun() { out.println("notFun");  }   
+   public void weird() { out.println("weird");  }   
+}
+
 public class InterfaceExtends
 {
   public static void main ( String[] args )
