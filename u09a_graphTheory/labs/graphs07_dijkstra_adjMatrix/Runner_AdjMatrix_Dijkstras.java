b671f0a38a0255007d9f5bb5ad67264aee815634
WeldonJasik <weldon.jasik@cfisd.net>
2025-05-01 07:54:31 -0500

    Updated GT lab 7 to read from a data file

--- u09a_graphTheory/labs/graphs07_dijkstra_adjMatrix/Runner_AdjMatrix_Dijkstras.java
+++ u09a_graphTheory/labs/graphs07_dijkstra_adjMatrix/Runner_AdjMatrix_Dijkstras.java
@@ -1,4 +1,7 @@
+//package graphs6b_dijkstra_matrix;
+
 import static java.lang.System.*;
+//import static graphs6b_dijkstra_matrix.Letters.LETTERS;
 import java.util.*;
 import java.io.*;
 
@@ -6,6 +9,7 @@ public class Runner_AdjMatrix_Dijkstras {
 
        
    public static void main(String[] args) {
+  
                        // A  B  C  D  E  F
       int[][] matrix1 = {{0, 0, 2, 8, 3, 0}, // A
                          {0, 0, 0, 6, 0, 0}, // B
@@ -14,48 +18,6 @@ public class Runner_AdjMatrix_Dijkstras {
                          {0, 0, 0, 0, 0, 0}, // E is a sink
                          {1, 0, 0, 0, 0, 0}  // F is
                         };         //    ^----- a source
-    
-                       //  A   B   C   D   E   F
-      int[][] matrix2 = {{  0, 22,  0, 15,  3,  0},  // A
-                         {  0,  0,  0,  4,  0,  0},  // B
-                         {  0,  1,  0,  0,  0,  0},  // C
-                         {  0,  0, 25,  0,  0,  6},  // D
-                         {  0,  0,  0,  2,  0,  0},  // E
-                         {  0,  0,  8,  0,  0,  0},  // F
-                        };
-   
-     // Same as matrix2, but symmetric since it's an undirected graph.
-     
-                       //  A   B   C   D   E   F
-      int[][] matrix4 = {{  0, 22,  0, 15,  3,  0},  // A
-                         { 22,  0,  1,  4,  0,  0},  // B
-                         {  0,  1,  0, 25,  0,  8},  // C
-                         { 15,  4, 25,  0,  2,  6},  // D
-                         {  3,  0,  0,  2,  0,  0},  // E
-                         {  0,  0,  8,  6,  0,  0},  // F
-                        };    
-                        
-                                     //  A    B    C    D    E    F    G
-      int[][] matrix5 = new int[][]{ {   0,   7,   0,   5,   0,   0,   0 },
-                                     {   7,   0,   8,   9,   7,   0,   0 },
-                                     {   0,   8,   0,   0,   5,   0,   0 },
-                                     {   5,   9,   0,   0,  15,   6,   0 },
-                                     {   0,   7,   5,  15,   0,   8,   9 },
-                                     {   0,   0,   0,   6,   8,   0,  11 },
-                                     {   0,   0,   0,   0,   9,  11,   0 },
-                                   };
-                                      
-                                      //  A    B    C    D    E    F    G    H
-      int[][] matrix6 = new int[][]{  {   0,   6,   1,   3,   0,   0,   5,   0 },
-                                      {   6,   0,   0,   0,   3,   0,   6,   3 },
-                                      {   1,   0,   0,   1,   0,   0,   0,   0 },
-                                      {   3,   0,   1,   0,   2,   8,   0,   0 },
-                                      {   0,   3,   0,   2,   0,   0,   0,   4 },
-                                      {   0,   0,   0,   8,   0,   0,   0,   5 },
-                                      {   5,   6,   0,   0,   0,   0,   0,   0 },
-                                      {   0,   3,   0,   0,   4,   5,   0,   0 },
-                                     };
-   
       out.println("Graph one:");
       AdjMatrix_Dijkstras graph1 = new AdjMatrix_Dijkstras(matrix1);
       out.println(graph1);
@@ -65,75 +27,115 @@ public class Runner_AdjMatrix_Dijkstras {
       graph1.printDfsIter_Forward(5);      
       out.print("DFS iter <-:   ");
       graph1.printDfsIter_Backward(5);         
-      out.print("BFS: ");
+      out.print("BFS:           ");
       graph1.printBFS(5);
       out.println("\n");
-      out.println("Dijkstra's starting at F:");
+      out.println("Dijkstra's Algorithm starting at F:");
       graph1.dijsktras(5);
-         
+      out.println();
+       
+                       //  A   B   C   D   E   F
+      int[][] matrix2 = {{  0, 22,  0, 15,  3,  0},  // A
+                         {  0,  0,  0,  4,  0,  0},  // B
+                         {  0,  1,  0,  0,  0,  0},  // C
+                         {  0,  0, 25,  0,  0,  6},  // D
+                         {  0,  0,  0,  2,  0,  0},  // E
+                         {  0,  0,  8,  0,  0,  0},  // F
+                        };
       out.println("Graph two:");
       AdjMatrix_Dijkstras graph2 = new AdjMatrix_Dijkstras(matrix2);
       out.println(graph2);
-      out.print("DFS recursive: ");
+      out.print("DFS recursive starting at A: ");
       graph2.printDfsRecur(0);
-      out.print("DFS iter ->:   ");
+      out.print("DFS iter -> starting at A:   ");
       graph2.printDfsIter_Forward(0);      
-      out.print("DFS iter <-:   ");
+      out.print("DFS iter <- starting at A:   ");
       graph2.printDfsIter_Backward(0);         
-      out.print("BFS: ");
+      out.print("BFS starting at D:           ");
       graph2.printBFS(3);
       out.println("\n");
-      out.println("Dijkstra's starting at A:");
+      out.println("Dijkstra's Algorithm starting at A:");
       graph2.dijsktras(0);
       out.println("\n");
-      out.println("Dijkstra's starting at B:");
+      out.println("Dijkstra's Algorithm starting at B:");
       graph2.dijsktras(1);            
-                            
+      out.println();
+
+     // Same as matrix2, but symmetric since it's an undirected graph.
+     
+                        //  A   B   C   D   E   F
+      int[][] matrix3 = {{  0, 22,  0, 15,  3,  0},  // A
+                         { 22,  0,  1,  4,  0,  0},  // B
+                         {  0,  1,  0, 25,  0,  8},  // C
+                         { 15,  4, 25,  0,  2,  6},  // D
+                         {  3,  0,  0,  2,  0,  0},  // E
+                         {  0,  0,  8,  6,  0,  0},  // F
+                        };    
+                        
+      out.println("Graph three:");
+      AdjMatrix_Dijkstras graph3 = new AdjMatrix_Dijkstras(matrix3);
+      out.println(graph3);
+      out.print("DFS recursive starting at C: ");
+      graph3.printDfsRecur(2);
+      out.print("DFS iter -> starting at C:   ");
+      graph3.printDfsIter_Forward(2);      
+      out.print("DFS iter <- starting at C:   ");
+      graph3.printDfsIter_Backward(2);         
+      out.print("BFS starting at F: ");
+      graph3.printBFS(5);
+      out.println("\n");
+      out.println("Dijkstra's Algorithm starting at E:");
+      graph3.dijsktras(4);
+      out.println();
+
+                                     //  A    B    C    D    E    F    G
+      int[][] matrix4 = new int[][]{ {   0,   7,   0,   5,   0,   0,   0 },
+                                     {   7,   0,   8,   9,   7,   0,   0 },
+                                     {   0,   8,   0,   0,   5,   0,   0 },
+                                     {   5,   9,   0,   0,  15,   6,   0 },
+                                     {   0,   7,   5,  15,   0,   8,   9 },
+                                     {   0,   0,   0,   6,   8,   0,  11 },
+                                     {   0,   0,   0,   0,   9,  11,   0 },
+                                   };
       out.println("Graph four:");
       AdjMatrix_Dijkstras graph4 = new AdjMatrix_Dijkstras(matrix4);
       out.println(graph4);
-      out.print("DFS recursive starting at C: ");
-      graph4.printDfsRecur(2);
-      out.print("DFS iter -> starting at C:   ");
-      graph4.printDfsIter_Forward(2);      
-      out.print("DFS iter <- starting at C:   ");
-      graph4.printDfsIter_Backward(2);         
-      out.print("BFS starting at F: ");
-      graph4.printBFS(5);
+      out.print("DFS recursive starting at A: ");
+      graph4.printDfsRecur(0);
+      out.print("DFS iter -> starting at A:   ");
+      graph4.printDfsIter_Forward(0);      
+      out.print("DFS iter <- starting at A:   ");
+      graph4.printDfsIter_Backward(0);         
+      out.print("BFS starting at A:           ");
+      graph4.printBFS(0);
       out.println("\n");
-      out.println("Dijkstra's starting at E:");
-      graph4.dijsktras(4);
-      out.println("\n");
-                                           
-      out.println("Graph five:");
+      out.println("Dijkstra's Algorithm starting at A:  ");
+      graph4.dijsktras(0);
+      out.println();
+
+                                      //  A    B    C    D    E    F    G    H
+      int[][] matrix5 = new int[][]{  {   0,   6,   1,   3,   0,   0,   5,   0 },
+                                      {   6,   0,   0,   0,   3,   0,   6,   3 },
+                                      {   1,   0,   0,   1,   0,   0,   0,   0 },
+                                      {   3,   0,   1,   0,   2,   8,   0,   0 },
+                                      {   0,   3,   0,   2,   0,   0,   0,   4 },
+                                      {   0,   0,   0,   8,   0,   0,   0,   5 },
+                                      {   5,   6,   0,   0,   0,   0,   0,   0 },
+                                      {   0,   3,   0,   0,   4,   5,   0,   0 },
+                                     };
+      out.println("\nGraph five:");
       AdjMatrix_Dijkstras graph5 = new AdjMatrix_Dijkstras(matrix5);
       out.println(graph5);
-      out.print("DFS recursive: ");
+      out.print("DFS recursive starting at A: ");
       graph5.printDfsRecur(0);
-      out.print("DFS iter ->:   ");
+      out.print("DFS iter -> starting at A:   ");
       graph5.printDfsIter_Forward(0);      
-      out.print("DFS iter <-:   ");
+      out.print("DFS iter <- starting at A:   ");
       graph5.printDfsIter_Backward(0);         
-      out.print("BFS: ");
+      out.print("BFS starting at A:           ");
       graph5.printBFS(0);
       out.println("\n");
-      out.println("Dijkstra's starting at A:");
+      out.println("Dijkstra's Algorithm starting at A:");
       graph5.dijsktras(0);
-      
-      out.println("\nGraph six:");
-      AdjMatrix_Dijkstras graph6 = new AdjMatrix_Dijkstras(matrix6);
-      out.println(graph6);
-      out.print("DFS recursive: ");
-      graph6.printDfsRecur(0);
-      out.print("DFS iter ->:   ");
-      graph6.printDfsIter_Forward(0);      
-      out.print("DFS iter <-:   ");
-      graph6.printDfsIter_Backward(0);         
-      out.print("BFS: ");
-      graph6.printBFS(0);
-      out.println("\n");
-      out.println("Dijkstra's starting at A:");
-      graph6.dijsktras(0);
    }
 }
-
