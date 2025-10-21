7435a4fedfd832f5ba472b27c34f88613ceb9ac2
WeldonJasik <weldon.jasik@cfisd.net>
2025-10-21 10:06:59 -0500

    Updated bitwise notes

--- u05c_binary_and_bitwise/notes/BitwiseAnd.java
+++ u05c_binary_and_bitwise/notes/BitwiseAnd.java
@@ -1,16 +1,16 @@
-//© A+ Computer Science
+//Â© A+ Computer Science
 // www.apluscompsci.com
 
-//bitwise or example
+//bitwise and example
 
 import static java.lang.System.*;
 
-public class BitwiseXOr
+public class BitwiseAnd
 {
 	public static void main(String args[])
 	{
 		int one=8;   //binary 1000
 		int two=7;   //binary 0111
-		out.println("8 ^ 7 == " + (one^two));
+		out.println("8 & 7 == " + (one&two));
 	}
 }
