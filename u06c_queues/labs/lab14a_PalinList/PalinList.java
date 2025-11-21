6ba389d3919acca731ea9ae5af58fa65f48d3fcd
WeldonJasik <weldon.jasik@cfisd.net>
2025-11-21 14:17:33 -0600

    update queue notes

--- u06c_queues/labs/lab14a_PalinList/PalinList.java
+++ u06c_queues/labs/lab14a_PalinList/PalinList.java
@@ -1,38 +1,17 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class -
-//Lab  -
-
+import java.util.Deque;
 import java.util.Queue;
 import java.util.Stack;
-import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class PalinList
-{
-	private Queue<String> queue;
-	private Stack<String> stack;
+public class PalinList {
+   // Use a queue and stack to determine whether the string
+   // represents a palindrome.
+   public static boolean isPalin(String list) {
 
-	public PalinList()
-	{
-		setList("");
-	}
-
-	public PalinList(String list)
-	{
-	}
-
-	public void setList(String list)
-	{
-	}
-
-	public boolean isPalin()
-	{
-		return true;
-	}
-
-
-	//write a toString method
+      return false;
+   }
+   
+   /* Once you've implemented isPalin using a stack and queue,
+    * try to do so using just a stack. Then try using a deque.
+    */
 }
