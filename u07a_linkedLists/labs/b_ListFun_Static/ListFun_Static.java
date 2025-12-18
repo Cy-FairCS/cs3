66b45960cc32a38d058a6a75e23db51e7556deed
WeldonJasik <weldon.jasik@cfisd.net>
2025-12-18 07:27:58 -0600

    Updated LL lab B

--- u07a_linkedLists/labs/b_ListFun_Static/ListFun_Static.java
+++ u07a_linkedLists/labs/b_ListFun_Static/ListFun_Static.java
@@ -6,12 +6,12 @@
 
 import static java.lang.System.*;
 
-public class StaticListFunHouse {
+public class ListFun_Static {
    /* Adding this private constructor means a default public constructor
     * won't exist and users of this class will have to call the methods
     * using the class name since they aren't able to construct objects.
     */
-   private StaticListFunHouse() {   }
+   private ListFun_Static() {   }
    
    //this method will print the entire list on the screen
    public static void print(ListNode list) {
